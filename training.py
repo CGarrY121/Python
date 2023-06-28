615e1e3fffebfe8397e9701b36c509eff6ad8e22
Berlous Ivan <huawei121121@gmail.com>
2023-06-28 14:53:15 +0500

    первая версия

---
+++ training.py
@@ -0,0 +1,16 @@
+inf = {"gorod" : "EKB",
+       "Ulica" : "polenina",
+      "dom" : 38}
+print (inf["gorod"])
+inf ["dom"] -= 10
+print (inf)
+inf ["kv"] = 33
+print (inf)
+
+avto = ["Lada granta", " Gazel" , "toyota korova"]
+print(avto)
+print (avto[-1])
+avto.append ("govnovoz")
+print(avto)
+del (avto[-1])
+print (avto)
